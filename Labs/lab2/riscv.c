c3d617e314a79182e96f758c17542bc72616eda6
emiislas <qeislas@gmail.com>
2021-10-10 22:09:30 -0600

    Last Update

--- Labs/lab2/riscv.c
+++ Labs/lab2/riscv.c
@@ -8,8 +8,17 @@
 int32_t* reg; // Array of 32 32-bit registers
 
 void init_regs();
-bool interpret(char* instr);
-void write_read_demo();
+bool interpret(char** tokens);
+void print_regs();
+bool equal(char str1[], char str2[]);
+char** make_token(char instruction[]);
+
+int regd; //dest
+int reg1; //register 1
+int reg2; //register 2
+int imm;  // immediate value
+int address; //current address
+char* mem = "mem.txt"; 
 
 /**
  * Initialize register array for usage.
@@ -24,23 +33,92 @@ void init_regs(){
 }
 
 
+/*
+This method takes in an array of tokens, each token is a segment of the current instruction,
+If the token at index 0 is either ADD, ADDI, LW, or SW it will interpret the 
+instruction and return true.
+If it not any of the listed instructions it will only return false.*/
 
-/**
- * Fill out this function and use it to read interpret user input to execute RV64 instructions.
- * You may expect that a single, properly formatted RISC-V instruction string will be passed
- * as a parameter to this function.
- */
-bool interpret(char* instr){
+bool interpret(char** token){
+	
+
+	if(equal(token[0], "ADD")){ 
+		regd = atoi(token[1]); 
+		reg1 = atoi(token[2]);
+		reg2 = atoi(token[3]); 
+			
+		reg[regd] = reg[reg1] + reg[reg2]; 
+		
+	}
+
+	else if(equal(token[0], "ADDI")){
+		regd = atoi(token[1]);
+		reg1 = atoi(token[2]); 
+		imm = atoi(token[3]);
+			
+		reg[regd] = reg[reg1] + imm; 
+		
+	}
+
+	else if(equal(token[0], "LW")){
+		regd = atoi(token[1]); 
+		reg1 = atoi(token[2]);
+		imm = atoi(token[3]); 
+			
+		address = reg[reg1] + imm; 
+		
+		int32_t read = read_address(address, mem);
+		
+		reg[regd] = read; 
+		
+	}
+
+	else if(equal(token[0], "SW")){
+		regd = atoi(token[1]); 
+		reg1 = atoi(token[2]);
+		imm = atoi(token[3]);
+			
+			
+		address = reg[reg1] + imm; 
+		
+		write_address(reg[regd], address, mem); 
+		
+		
+	}
+	else {
+		printf("Incorrect Input\n");
+		return false;
+	}
 	return true;
 }
 
 
-/**
- * Simple demo program to show the usage of read_address() and write_address() found in memory.c
- * Before and after running this program, look at mem.txt to see how the values change.
- * Feel free to change "data_to_write" and "address" variables to see how these affect mem.txt
- * Use 0x before an int in C to hardcode it as text, but you may enter base 10 as you see fit.
- */
+/*This method prints the array of registers given*/
+void print_regs(){
+    int col_size = 10;
+    for(int i = 0; i < 8; i++){
+        printf("X%02i:%.*lld", i, col_size, (long long int) reg[i]);
+        printf(" X%02i:%.*lld", i+8, col_size, (long long int) reg[i+8]);
+        printf(" X%02i:%.*lld", i+16, col_size, (long long int) reg[i+16]);
+        printf(" X%02i:%.*lld\n", i+24, col_size, (long long int) reg[i+24]);
+    }
+}
+//compare strings
+bool equal(char str1[], char str2[]){
+	
+	int i = 0;
+	while(str1[i] != '\0'){
+		if(str1[i] != str2[i]){
+			return false;
+		}
+		i++;
+	}
+	return true; 
+}
+
+
+
+/*
 void write_read_demo(){
 	int32_t data_to_write = 0xFFF; // equal to 4095
 	int32_t address = 0x98; // equal to 152
@@ -54,17 +132,28 @@ void write_read_demo(){
 
 	printf("Read address %lu (0x%lX): %lu (0x%lX)\n", address, address, read, read); // %lu -> format as an long-unsigned
 }
+*/
 
-/**
- * Your code goes in the main
- *
- */
 int main(){
-	// Do not write any code between init_regs
-	init_regs(); // DO NOT REMOVE THIS LINE
-
-	// Below is a sample program to a write-read. Overwrite this with your own code.
-	write_read_demo();
+	
+	init_regs(); 
+	print_regs();
+	char input[16];
+	char** tokens;
+	bool term = true;
+	
+	while(term){
+		
+		printf("Input instructions (Only Caps): ");
+		fgets(input, sizeof(input), stdin);
+		
+		tokens = tokenize(input);
+		term = interpret(tokens); //main function
+		
+		if(term){
+			print_regs();
+		}
+	}
 
 	return 0;
 }
